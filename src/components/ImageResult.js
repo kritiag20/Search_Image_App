7294c316511129112e92babc6e4430f393ae98db
Kriti Agarwal <kritiagarwal2098@gmail.com>
2021-05-13 23:41:30 +0530

    styling to set like n dislike button
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

--- src/components/ImageResult.js
+++ src/components/ImageResult.js
@@ -3,6 +3,8 @@ import { IconButton, Button, Dialog, DialogActions } from '@material-ui/core';
 // import { GridListTile, GridList, GridListTileBar } from '@material-ui/core';
 import ZoomInIcon from '@material-ui/icons/ZoomIn';
 import NotFound from '../Icons/NotFound.svg';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 class ImageResult extends Component {
 
@@ -11,7 +13,9 @@ class ImageResult extends Component {
 
         this.state = {
             open: false,
-            currentImg: ''
+            currentImg: '',
+            likeIcon: null,
+            dislike: true
         }
         this.handleOpen = this.handleOpen.bind(this)
         this.handleClose = this.handleClose.bind(this)
@@ -24,12 +28,25 @@ class ImageResult extends Component {
     handleClose = () => {
         this.setState({ open: false })
     }
+    handlelike = (id) => {
+        this.setState({ likeIcon: id , dislike : !this.state.dislike})
+    }
 
     render() {
 
         let ImgList
         const images = this.props.result
         console.log("images", images)
+        
+        let ShowLikeIcon = ''
+        let id = this.state.likeIcon
+        for (let imgId = 0; imgId < images.length; imgId++) {
+            if (id === images[imgId].id) {
+                ShowLikeIcon = images[imgId].id
+                break
+            }
+        }
+
         if (images) {
             ImgList = (
                 <div className="displayImg">
@@ -38,8 +55,12 @@ class ImageResult extends Component {
                             <div className="image">
                                 <img className="imgSize" src={image.webformatURL} alt="" />
                                 <div className="pop">
+
                                     <div className="zoomIconCont" onClick={() => this.handleOpen(image.webformatURL)}>
                                         <ZoomInIcon className="zoomBtn" />
+                                    </div>
+                                    <div className="zoomIconCont likeBtn" onClick={() => this.handlelike(image.id)}>
+                                        {ShowLikeIcon === image.id && this.state.dislike ? <FavoriteIcon className="LikeIcon zoomBtn"/> : <FavoriteBorderIcon className="zoomBtn"/>}
                                     </div>
                                 </div>
                             </div>
@@ -61,7 +82,7 @@ class ImageResult extends Component {
                 <div>
                     {ImgList}
                 </div>
-                <Dialog open={this.state.open}>
+                <Dialog className="modal" open={this.state.open}>
                     <img onClick={() => this.handleClose()} className="imghover" src={this.state.currentImg} alt="" />
                     <DialogActions>
                         <Button autoFocus onClick={() => this.handleClose()} color="primary">
